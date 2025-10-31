3943a43209b5c2a4fded1a1155fdf6144e900757
Sufyan <trainee.sufyan.shah@sigmalabs.co.uk>
2025-10-31 17:15:09 +0000

    Assessment Complete!

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -1,8 +1,8 @@
+'''Here the Trainee, Assessment, Quiz, Marking and Question Classes are initialised'''
 from datetime import date
 
 class Trainee():
     '''Trainee Class'''
-
     def __init__(self, name: str, email: str, date_of_birth: date):
         self.name = name
         self.email = email
@@ -38,7 +38,6 @@ class Trainee():
 
 class Assessment:
     '''Assessment Class'''
-
     def __init__(self, name: str, type: str, score: float):
         self.name = name
         self.type = type
@@ -54,6 +53,7 @@ class Assessment:
 
 
 class MultipleChoiceAssessment(Assessment):
+    '''Creates Multiple Choice class that inherits assessment'''
     def __init__(self, name: str, score: float):
         super().__init__(name, "multiple-choice", score)
 
@@ -62,6 +62,7 @@ class MultipleChoiceAssessment(Assessment):
 
 
 class TechnicalAssessment(Assessment):
+    '''Creates Techinial class that inherits assessment'''
     def __init__(self, name: str, score: float):
         super().__init__(name, "technical", score)
 
@@ -70,15 +71,16 @@ class TechnicalAssessment(Assessment):
 
 
 class PresentationAssessment(Assessment):
+    '''Creates Presentation class that inherits assessment'''
     def __init__(self, name: str, score: float):
         super().__init__(name, "presentation", score)
 
     def calculate_score(self):
-        return (self.score * 0.6)
+        return self.score * 0.6
 
 
 class Question:
-
+    '''Question class for creating questions and answers'''
     def __init__(self, question: str, chosen_answer: str, correct_answer: str):
         self.question = question
         self.chosen_answer = chosen_answer
@@ -86,7 +88,7 @@ class Question:
 
 
 class Quiz:
-
+    '''Quiz class for generating quiz'''
     def __init__(self, questions: list, name: str, type: str):
         self.questions = questions
         self.name = name
@@ -94,23 +96,32 @@ class Quiz:
 
 
 class Marking:
-
+    '''Marking Class handles the marking'''
     def __init__(self, quiz: Quiz) -> None:
         self._quiz = quiz
-    
-    def mark(self) -> int: 
+
+    def mark(self) -> int:
+        '''Mark method used to check if answers are correct and gives scores'''
         total_score = 0
-        for question in self._quiz.questions: 
+        for question in self._quiz.questions:
             if question.chosen_answer == question.correct_answer:
                 total_score += 1
 
-        
         calculated_percentage = (total_score/len(self._quiz.questions)) * 100
         return int(calculated_percentage)
 
 
-    def generate_assessment(self) -> Assessment: 
-        
+    def generate_assessment(self) -> Assessment:
+        '''Returns instance of assessment'''
+        score = self.mark()
+
+        if self._quiz.type.lower() == "multiple-choice":
+            return MultipleChoiceAssessment(self._quiz.name, score)
+        elif self._quiz.type.lower() == "technical":
+            return TechnicalAssessment(self._quiz.name, score)
+        else:
+            return Assessment(self._quiz.name, score)
+
 
 if __name__ == "__main__":
     # Example questions and quiz
